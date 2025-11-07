cbfc283954ac4a4c8274520712c7762f9ca27012
kira <atsushikira0826@gmail.com>
2025-11-07 16:07:18 +0900

    サイト名の変更と画像の挿入など

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'TechBulkのブログ';
+export const SITE_DESCRIPTION = '情報工学部生TechBulkの学習アウトプットブログ。コンピュータサイエンスの基礎理論からWeb開発、日々の学びを記録します';
